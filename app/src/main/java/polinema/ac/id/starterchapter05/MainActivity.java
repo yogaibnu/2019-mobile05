0eab9e68142e8f37e58fc20731839dfc681918da
yogaibnu <yogaibnudinataa@gmail.com>
2019-09-23 14:30:59 +0700

    embedded fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,11 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activities.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handlerStaticFragment(View view) {
+        Intent intent = new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerDynamicFragment(View view) {
+    }
+
+    public void handlerPraktikumFragment(View view) {
     }
 }
